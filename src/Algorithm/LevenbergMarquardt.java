df4dce2fb85a21a73cc5a93e74135e8592812413
Wai Pai Lee <pailee.wai@gmail.com>
2017-09-21 16:06:58 +0800

    initial commit

---
+++ src/Algorithm/LevenbergMarquardt.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Algorithm;
+
+import Jama.Matrix;
+
+/**
+ *
+ * @author Wai Pai Lee
+ */
+public class LevenbergMarquardt {
+    
+}
